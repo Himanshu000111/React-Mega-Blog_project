cfc1d71f1ee90d181023905019423ca55d546852
Himanshu000111 <145653053+Himanshu000111@users.noreply.github.com>
2024-06-12 21:20:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
+import { Editor } from '@tinymce/tinymce-react';
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -33,6 +34,14 @@ function App() {
       </div>
     </div>
   ) : null
+      <Editor
+      apiKey='a4evxsojllczgvluk8c3e79dzgej6dmcue8ee86ibc9aefrn'
+      init={{
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+      }}
+      initialValue="Welcome to TinyMCE!"
+    />
 }
 
 export default App
